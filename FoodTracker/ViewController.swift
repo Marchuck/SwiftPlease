1fd912143d6cbf7cffb05812a8ff23a860a1e640
lukasz <lukmar993@gmail.com>
2016-08-11 23:18:58 +0200

    working demo app:

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var labelRef: UILabel!
+    
+    @IBOutlet weak var textViewRef: UITextField!
+    
+    
+    @IBAction func onButtonClick(sender: AnyObject) {
+        labelRef?.text =textViewRef?.text;
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
