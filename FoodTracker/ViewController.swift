18eb42afefff663488b0255c5c91425b78738794
lukasz <lukmar993@gmail.com>
2016-08-15 13:04:55 +0200

    working image selection

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,28 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-   
+    @IBOutlet weak var photoImageView: UIImageView!
+
     @IBOutlet weak var labelRef: UILabel!
     
     @IBOutlet weak var textViewRef: UITextField!
     
-    
     @IBAction func onButtonClick(sender: AnyObject) {
-        labelRef?.text =textViewRef?.text;
-        
+        print("onButtonClick");
+        labelRef!.text = textViewRef!.text;
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textViewRef.delegate = self;
+        setupImageClickListener();
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupImageClickListener() {
+        let tapGestureRecognizer
+            = UITapGestureRecognizer(target:self, action:#selector(ViewController.selectImageFromPhotoLibrary(_:)));
+        photoImageView.userInteractionEnabled = true
+        photoImageView.addGestureRecognizer(tapGestureRecognizer)
     }
+    
+    //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        print("selectImageFromPhotoLibrary");
+        textViewRef?.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("imagePickerController");
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
-
+    func textFieldDidEndEditing(textField: UITextField){
+        print("textFieldDidEndEditing: " + String(textViewRef.text));
+        labelRef.text = textViewRef.text;
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn");
+        textViewRef?.resignFirstResponder();
+        return true
+    }
 }
 
